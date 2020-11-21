cc88f3942f28f09a94a0d11ce73745215adcb619
Renier Meintjies <reniermeintjies@gmail.com>
2020-11-21 15:49:57 +0200

    change template

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -8,8 +10,10 @@ import (
 	"github.com/joho/godotenv"
 )
 
+var tpl = template.Must(template.ParseFiles("index.html"))
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello World!</h1>"))
+	tpl.Execute(w, nil)
 }
 
 func main() {
@@ -26,6 +30,7 @@ func main() {
 	}
 
 	mux := http.NewServeMux()
+	fmt.Println("server running on port 3000!!!")
 
 	mux.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":"+port, mux)
