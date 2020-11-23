5e132842947d684fe19f06472e6067b74b59a456
Renier Meintjies <reniermeintjies@gmail.com>
2020-11-23 18:23:53 +0200

    add news

--- news/news.go
+++ news/news.go
@@ -2,57 +2,12 @@ package news
 
 import (
 	"encoding/json"
-
 	"fmt"
-
 	"io/ioutil"
-
 	"net/http"
-
 	"net/url"
-
 	"time"
 )
-
-type Client struct {
-	http     *http.Client
-	key      string
-	PageSize int
-}
-
-func (c *Client) FetchEverything(query, page string) (*Results, error) {
-
-	endpoint := fmt.Sprintf("https://newsapi.org/v2/everything?q=%s&pageSize=%d&page=%s&apiKey=%s&sortBy=publishedAt&language=en", url.QueryEscape(query), c.PageSize, page, c.key)
-
-	resp, err := c.http.Get(endpoint)
-
-	if err != nil {
-
-		return nil, err
-
-	}
-
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-
-		return nil, err
-
-	}
-
-	if resp.StatusCode != http.StatusOK {
-
-		return nil, fmt.Errorf(string(body))
-
-	}
-
-	res := &Results{}
-
-	return res, json.Unmarshal(body, res)
-
-}
 
 type Article struct {
 	Source struct {
@@ -68,8 +23,49 @@ type Article struct {
 	Content     string    `json:"content"`
 }
 
+func (a *Article) FormatPublishedDate() string {
+	year, month, day := a.PublishedAt.Date()
+	return fmt.Sprintf("%v %d, %d", month, day, year)
+}
+
 type Results struct {
 	Status       string    `json:"status"`
 	TotalResults int       `json:"totalResults"`
 	Articles     []Article `json:"articles"`
+}
+
+type Client struct {
+	http     *http.Client
+	key      string
+	PageSize int
+}
+
+func (c *Client) FetchEverything(query, page string) (*Results, error) {
+	endpoint := fmt.Sprintf("https://newsapi.org/v2/everything?q=%s&pageSize=%d&page=%s&apiKey=%s&sortBy=publishedAt&language=en", url.QueryEscape(query), c.PageSize, page, c.key)
+	resp, err := c.http.Get(endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf(string(body))
+	}
+
+	res := &Results{}
+	return res, json.Unmarshal(body, res)
+}
+
+func NewClient(httpClient *http.Client, key string, pageSize int) *Client {
+	if pageSize > 100 {
+		pageSize = 100
+	}
+
+	return &Client{httpClient, key, pageSize}
 }
